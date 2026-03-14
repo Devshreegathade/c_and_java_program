d44c734bd971e63e70f51cca3dd4d74c1fa2c50a
DEVSHREE  SUDHIR GATHADE <gdevshree01@gmail.com>
2026-03-14 22:50:11 +0530

    Rename AgeCalculator class to dobToAge

--- dobToAge.java
+++ dobToAge.java
@@ -2,7 +2,7 @@ import java.time.LocalDate;
 import java.time.Period;
 import java.util.Scanner;
 
-public class AgeCalculator {
+public class dobToAge {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your birthdate (YYYY-MM-DD): ");
